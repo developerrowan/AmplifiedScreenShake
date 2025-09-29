24d0a22ea95c8396b9769ca619c93d5604ae9970
Devin Rowan <its@autumnrowan.com>
2025-09-28 19:54:35 -0500

    Update README, add mod

---
+++ amplified-screen-shake.lua
@@ -0,0 +1,11 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Amplied Screen Shake
+--- MOD_ID: AmpliedScreenShake
+--- MOD_AUTHOR: [DeveloperRowan]
+--- MOD_DESCRIPTION: With ASS, you can set your screenshake unreasonably high.
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+----------------------------------------------
+------------MOD CODE END----------------------
